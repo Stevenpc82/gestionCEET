8ede7e9f690d040b792d0a0c2f49399e82386729
Stiven Pineda <jspineda74@misena.edu.co>
2021-11-07 12:04:03 -0500

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ (Auth::check() && (Auth::user()->role == 'student' || Auth::user()->role == 'teacher'
+        || Auth::user()->role == 'admin' || Auth::user()->role == 'accountant' || Auth::user()->role ==
+        'librarian'))?Auth::user()->school->name:config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+
+    <link rel="stylesheet" href="{{ url('css/loader.css') }}">
+
+    <script src="{{ url('js/vendors.js') }}"></script>
+
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    <script src="{{ url('js/application.js') }}"></script>
+    @yield('after_scripts')
+</head>
+
+<body>
+@include('components.loader')
+<div id="app">
+    @include('components.navbar-top')
+    @yield('content')
+</div>
+
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons&style=normal&weight=400"
+      rel="stylesheet">
+<link rel="stylesheet" href="{{ url('css/vendors.css') }}" id="bootswatch-print-id">
+<link rel="stylesheet" href="{{ url('css/application.css') }}">
+</body>
+
+</html>
